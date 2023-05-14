b3f8432c446e36df070faaa5df134d80e139ea91
Phillip Sitbon <phillip.sitbon@gmail.com>
2023-05-13 19:55:46 -0700

    finder: proper refcounting

--- lazi/core/finder.py
+++ lazi/core/finder.py
@@ -8,7 +8,7 @@ from importlib.abc import MetaPathFinder
 from contextlib import contextmanager
 
 from lazi.conf import conf
-from lazi.util import Singleton, debug
+from lazi.util import Singleton, classproperty, debug
 from .record import SpecRecord
 from .loader import Loader
 
@@ -18,8 +18,11 @@ __all__ = "Finder",
 class Finder(Singleton, MetaPathFinder):
     LoaderType: type[Loader] = Loader
     SpecRecordType: type[SpecRecord] = SpecRecord
-    __count__: int = 0
-    __skip__: int = 0
+    __refs__: int = 0
+    __skip: int = 0
+
+    instance = classproperty(lambda cls: cls.__instance__)
+    refs = classproperty(lambda cls: cls.__instance__.__refs__)
 
     @classmethod
     @contextmanager
@@ -39,28 +42,32 @@ class Finder(Singleton, MetaPathFinder):
         return cls.__instance__.__uninstall()
 
     def __install(self) -> Finder:
-        assert None is debug.trace(f"install {self.__class__.__name__}[{self.__count__}]")
+        assert None is debug.trace(f"install {self.__class__.__name__}[{self.__refs__}]")
 
         if self not in sys.meta_path:
             sys.meta_path.insert(0, self)
 
-        self.__count__ += 1
+        self.__refs__ += 1
         return self
 
-    def __uninstall(self) -> bool:
-        assert None is debug.trace(f"uninstall {self.__class__.__name__}[{self.__count__}]")
-        assert self.__count__ > 0
+    def __uninstall(self) -> Finder | None:
+        assert None is debug.trace(f"uninstall {self.__class__.__name__}[{self.__refs__}]")
+
+        if self.__refs__ == 0:
+            return None
+
+        assert self.__refs__ > 0
         assert self in sys.meta_path
 
-        self.__count__ -= 1
+        self.__refs__ -= 1
 
-        if self.__count__ == 0:
+        if self.__refs__ == 0:
             sys.meta_path.remove(self)
             self.invalidate_caches()
             super().__delete__()
-            return True
+            return None
 
-        return False
+        return self
 
     @classmethod
     def lazy_module(cls, name: str, path: list[str] | None = None, target: ModuleType | None = None) -> ModuleType:
@@ -74,7 +81,8 @@ class Finder(Singleton, MetaPathFinder):
         return (self := cls.__instance__).SpecRecordType.find(finder=self, name=name, path=path, target=target)
 
     def find_spec(self, fullname, path=None, target=None) -> ModuleSpec | None:
-        if self.__skip__ or not self.__count__:
+        assert self.__skip >= 0
+        if self.__skip:
             return None
 
         if conf.DEBUG_TRACING > 1:
@@ -88,11 +96,12 @@ class Finder(Singleton, MetaPathFinder):
 
         This is normally only called via SpecRecord.find() when a new record is created.
         """
-        self.__skip__ += 1
+        assert self.__skip >= 0
+        self.__skip += 1
         try:
             return find_spec(record.name)
         finally:
-            self.__skip__ -= 1
+            self.__skip -= 1
 
     def invalidate_caches(self) -> None:
         self.SpecRecordType.RECORD.clear()
