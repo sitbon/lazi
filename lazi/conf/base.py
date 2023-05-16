1204152bac2497ef5402826af7a5939db2f4e1ea
Phillip Sitbon <phillip.sitbon@gmail.com>
2023-05-16 00:05:10 -0700

    finder: use FORCE_LOAD_MODULE here, and enable soft invalidation & gc.

--- lazi/conf/base.py
+++ lazi/conf/base.py
@@ -1,18 +1,20 @@
 """Base project configuration.
 """
-
 __meta__ = dict(                        # Internal configuration for <root>.conf.conf (cannot override elsewhere).
     root="lazi",                        # - Root namespace package name.
 )                                       # See conf.py for more options.
-
-DEBUG_TRACING: int = 0                  # Enable debug traces.
-
+#
+DEBUG_TRACING: int = 0                  # Enable debug traces. Currently using levels 0-4.
+#
 LAZI_AUTO_INSTALL: bool = True          # Automatically install when importing lazi.auto.
 CORE_AUTO_INSTALL: bool = False         # Automatically install when importing lazi.core.
 FORCE_LOAD_MODULE: bool = False         # Immediately call exec_module() on imported modules.
 NO_STDLIB_MODULES: bool = True          # Disable loader hooking for stdlib modules.
 DISABLE_LOAD_HOOK: bool = False         # Disable all loader hooks.
-
+SOFT_INVALIDATION: bool = False         # Keep modules in sys.modules after cache invalidation.
+GARBAG_COLLECTION: bool = (             # Enable garbage collection on cache invalidation.
+    not SOFT_INVALIDATION)              # - Only makes sense if SOFT_INVALIDATION is False.
+#
 CONF_NO_CACHING: bool | None = None     # Disable caching of conf vars.
 #                                         - None: Use the default caching behavior, which will disable
 #                                                 caching if `lazi.core` is already present in sys.modules
--- lazi/core/finder.py
+++ lazi/core/finder.py
@@ -1,10 +1,12 @@
 from __future__ import annotations
 
+import gc
 import sys
 from types import ModuleType
 from importlib.util import find_spec, module_from_spec
 from importlib.abc import MetaPathFinder
 
+from lazi.conf import conf
 from lazi.util import classproperty, debug
 
 from .spec import Spec
@@ -19,9 +21,9 @@ class Finder(MetaPathFinder):
     Loader: type[Loader] = Loader
     Module: type[Module] = Module
 
+    __busy__: bool = False
     __refs__: int = 0
     __specs__: dict[str, Spec]
-
     __stack__: list[Finder] = []
 
     finders = classproperty(lambda cls: (_ for _ in sys.meta_path if isinstance(_, cls)))
@@ -37,6 +39,7 @@ class Finder(MetaPathFinder):
                 f"inst:{len(list(self.finders))} sys:{len(sys.meta_path)} "
             )
             sys.meta_path.insert(0, self)
+            self.__stack__.append(self)
 
         self.__refs__ += 1
         return self
@@ -50,6 +53,8 @@ class Finder(MetaPathFinder):
         self.__refs__ = max(self.__refs__ - 1, 0)
 
         if self.__refs__ == 0 and self in sys.meta_path:
+            pop = self.__stack__.pop()
+            assert pop is self, (pop, self)
             sys.meta_path.remove(self)
             self.invalidate_caches()
             return True
@@ -65,13 +70,27 @@ class Finder(MetaPathFinder):
         return __finder__.__exit__(None, None, None)
 
     def lazy(self, name: str, path: list[str] | None = None, target: ModuleType | None = None) -> ModuleType:
-        if (spec := self.find_spec(name, path, target)) is not None:
-            module = module_from_spec(spec)
-            spec.loader.exec_module(module)
-            return module
+        pop = False
+
+        if self not in self.__stack__:
+            self.__stack__.append(self)
+            pop = True
+
+        try:
+            if (spec := self.find_spec(name, path, target)) is not None:
+                module = module_from_spec(spec)
+                spec.loader.exec_module(module)
+                return module
+
+        finally:
+            if pop:
+                pop = self.__stack__.pop()
+                assert pop is self, (pop, self)
 
     def find_spec(self, name: str, path: list[str] | None = None, target: ModuleType | None = None) -> Spec | None:
-        if self in self.__stack__:
+        assert self in self.__stack__, (self, self.__stack__)
+
+        if self.__busy__ or self.__stack__[-1] != self:
             return None
 
         assert None is debug.traced(
@@ -84,20 +103,27 @@ class Finder(MetaPathFinder):
             assert spec.finder is self, (spec.finder, self)
             return spec
 
-        self.__stack__.append(self)
-
+        self.__busy__ = True
         try:
             if (spec := find_spec(name, path)) is not None:
                 spec = self.__specs__[name] = self.Spec(self, spec, path, target)
                 assert None is debug.traced(2, f"[{id(self)}] <foun> {spec.name} L:{spec.loader_state} o:{spec.origin}")
+                if conf.FORCE_LOAD_MODULE and (module := sys.modules.get(spec.name)):
+                    spec.loader.exec_module(module, spec, True)
                 return spec
-
         finally:
-            pop = self.__stack__.pop()
-            assert pop is self, (pop, self)
+            self.__busy__ = False
 
     def invalidate_caches(self) -> None:
-        self.__specs__.clear()
+        if not conf.SOFT_INVALIDATION:
+            while self.__specs__ and (spec := self.__specs__.popitem()[1]):
+                if hasattr(spec.loader, "unload_module"):
+                    spec.loader.unload_module(spec)
+        else:
+            self.__specs__.clear()
+
+        if conf.GARBAG_COLLECTION:
+            gc.collect()
 
 
 __finder__: Finder = Finder()
