4f6408422cafa2b86615e62a3f4fc79127509a45
Phillip Sitbon <phillip.sitbon@gmail.com>
2023-05-17 16:37:34 -0700

    test_lazy: use from inside test function.

--- tests/test_lazy.py
+++ tests/test_lazy.py
@@ -1,9 +1,11 @@
-from lazi.core import lazi
 
 
 def test_finder_lazy():
+    from lazi.core import lazi
 
     django = lazi.lazy("django")
     version = django.VERSION
     test = lazi.lazy("django.test")
     TestCase = test.TestCase
+
+    del lazi
